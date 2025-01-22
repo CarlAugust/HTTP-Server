4cc9c27c09e9d5e125dcc4ecb614cbee10f4bd16
carl <carlaugust@gmail.com>
2025-01-22 16:16:09 +0100

    I do not know what isnt working <_>...

--- server/main.c
+++ server/main.c
@@ -6,29 +6,17 @@
 #include <string.h>
 #include <errno.h>
 
-int main(int argc, char* argv[])
+int main()
 {
-    if (argc != 2)
-    {
-        printf("Not enough arguments\n");
-        return 0;
-    }
-
-    FILE* htmlPtr;
-
-    htmlPtr = fopen("index.html", "r");
-
-    char charBuffer[256];
-
-    fread(charBuffer, 256, 1, htmlPtr);
-
-    printf("%s", charBuffer);
-    fclose(htmlPtr);
-
-
     // Some testing
+    // if (argc != 2)
+    // {
+    //     printf("Not enough arguments\n");
+    //     return 0;
+    // }
+    // Change to commandline argument later
+    uint16_t port = 8081;
 
-    uint16_t port = atoi(argv[1]);
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1)
     {
@@ -71,23 +59,37 @@ int main(int argc, char* argv[])
             return 0;
         }
 
-
-        char httpBuffer[256];
-        read(client_fd, &httpBuffer, 256);
-        printf("Read data\n");
-        printf("%s\n\n", httpBuffer);
+        char requestBuffer[256];
+        read(client_fd, requestBuffer, 256);
+        printf("Response from client ---- \n%s\n\n", requestBuffer);
 
         // 5 since thats when the route begins in a http get request (GET /some route)
-        // for (int i = 5; i < 256, httpBuffer[i] != ' '; i++)
+        // for (int i = 5; i < 256, requestBuffer[i] != ' '; i++)
         // {
 
         // }
 
         // Return a valid http response
 
+        FILE* htmlPtr;
+
+        htmlPtr = fopen("index.html", "r");
+
+        char charBuffer[256];
+
+        int numRead = fread(charBuffer, 1, 256, htmlPtr); 
+        fclose(htmlPtr);
+
+        char response[512] = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n";
+
+        strcat(response, charBuffer);
+        printf("The servers response --- \n%s\n\n", response);
+
+        send(client_fd, response, numRead, 0);
 
         close(client_fd);
     }
 
+    close(fd);
     return 0;
 }
